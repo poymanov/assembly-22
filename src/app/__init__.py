eb506b457e7bd133f9a152d8abd4849c50270a9c
poymanov <n.poymanov@gmail.com>
2020-03-17 21:59:59 +0300

    Добавлена панель администратора

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 
 from app.config import Config
-from app.models import db
+from app.models import db, Participant, EventType, EventCategory, Event, Location, Enrollment
 from flask_migrate import Migrate
 from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
@@ -22,3 +23,10 @@ from app.views.v1.events import *
 from app.views.v1.event_types import *
 from app.views.v1.event_categories import *
 from app.views.v1.enrollments import *
+
+admin.add_view(ModelView(Location, db.session))
+admin.add_view(ModelView(Participant, db.session))
+admin.add_view(ModelView(EventType, db.session))
+admin.add_view(ModelView(EventCategory, db.session))
+admin.add_view(ModelView(Event, db.session))
+admin.add_view(ModelView(Enrollment, db.session))
